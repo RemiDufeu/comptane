cf597f9c6dbad92b32865081ac498a4512e3e874
RemsAlien <RemsAlien@users.noreply.github.com>
2020-05-26 13:54:54 +0200

    copie du fichier

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
+import {contact} from './contact'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import headercover from './img/hand-holding-pencil-at-meeting.jpg'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const contactindex = contact
+
+contactindex()
+const menu = [
+  {name : 'Notre programme', link : "/notreprogramme"},
+  {name : 'Nos offres', link : "/nosoffres"},
+  {name : 'contact', link : "contact"},
+  {name : 'version d\'éssai', link : "/notreprogramme"},
+]
+
+const homebar = menu.map((page) => 
+  <li key= {page.name}  className="homebarli"><a href= {page.link}>{page.name}</a></li>)
+
+function Login() {
+    return (
+      <div className="login">
+        <button>S'inscrire</button>
+        <button>Connexion</button>
+      </div>
+    )
+  }
+
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  
+  render() {
+      return ( 
+        <div>
+          <ul className="homebarul">{homebar} <Login/> </ul>
+          <img className="headcover" src={headercover} alt="comptabilité"></img>
+          
+        </div>
+        
+      );
+    }
+  }
+  
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Home/>,
+    document.getElementById('root')
+  );
